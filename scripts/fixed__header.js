b35d1eab8dd4475436331fe40c773354f125a934
JesterKira <programmerjester@gmail.com>
2022-06-27 22:00:18 +0300

    Add Intro section

--- scripts/fixed__header.js
+++ scripts/fixed__header.js
@@ -1,20 +1,26 @@
 window.onscroll = function fixHeader() {
-    const Header = document.querySelector('.header')
+    const Header = document.querySelector('.header');
+    const Main = document.querySelector('.main');
 
     if (window.innerWidth > 552){
         if (window.pageYOffset > 30) {
             Header.classList.add('fixed');
+            Main.style.paddingTop = Header.offsetHeight + 30 + 'px';
         } 
         else {
             Header.classList.remove('fixed');
+            Main.style.paddingTop = 0;
+
         }
     }
     else {
         if (window.pageYOffset > 20) {
             Header.classList.add('fixed');
+            Main.style.paddingTop = Header.offsetHeight + 20 + 'px';
         } 
         else {
             Header.classList.remove('fixed');
+            Main.style.paddingTop = 0;
         }
     }
 }
